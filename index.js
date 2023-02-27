9a150b1368ed0f4a9184a8844612cf477d737acd
unknown <bgandreev@gmail.com>
2023-02-27 16:23:31 -0600

    Added questions for the user to answer

---
+++ index.js
@@ -0,0 +1,57 @@
+const userQuestions = [
+  
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Please input your project name: ',
+    },
+  
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please briefly describe your project: ',
+    },
+  
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please select the license of your project: ',
+      choices: ['No License', 'GPL 3.0', 'BSD 3', 'MIT'],
+    },
+  
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please specify the command to install dependencies: ',
+    },
+  
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Please specify the command to run tests: ',
+    },
+  
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage information: ',
+    },
+    
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide information for contributing: ',
+    },
+  
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please input your GitHub username: ',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please input your email address: ',
+    },
+    
+  ];
